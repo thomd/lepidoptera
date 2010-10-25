8e8249bd50a3500f034ebadaf50e03db74804f14
Thomas Duerr <thomduerr@gmail.com>
2010-10-25 23:34:47 +0200

    parse reliably generator-group and -type from command line

--- lib/butterfly.rb
+++ lib/butterfly.rb
@@ -15,6 +15,8 @@ module Butterfly
     :git => false
   }
 
+  # basepath of generators
+  BASEPATH = File.join(File.dirname(__FILE__), '..', '*_generators')
 
 
   # Error handling
@@ -26,31 +28,46 @@ module Butterfly
   # Generator
   class CodeGenerators
     
-    attr_accessor :generators, :path
+    attr_accessor :generators
     
     def initialize
       @generators = []
-      @path = File.join(File.dirname(__FILE__), '..', '*_generators')
-      Dir[@path].each do |file|
-        @generators.push(Generator.new(file)) if File.directory?(file)
+      Dir[BASEPATH].each do |file|
+        @generators.push(GeneratorGroup.new(file)) if File.directory?(file)
       end
     end
     
+    # returns array of generator-group names
     def names
       generators.map{ |generator| generator.name }
     end
 
+    # returns array of generator-group sources
     def sources
-      generators.map{ |generator| RubiGen::PathSource.new(:app, generator.path) }
-      
+      generators.map{ |generator| generator.source }      
     end
     
-    # a generator contains a generator name and its path
-    class Generator
-      attr_accessor :name, :path
+    # returns array of generator-types for a given generator-group
+    def types_of(name)
+      selected = generators.select { |n| n.name == name }
+      if selected.length == 0
+        []
+      else
+        selected[0].types
+      end
+    end
+    
+    
+    # a generator-group contains a generator name, its path and its generator-types
+    class GeneratorGroup
+      
+      attr_accessor :name, :source, :types, :label
+      
       def initialize(file)
         @name = File.basename(file).gsub(/_generators$/, '')
-        @path = file
+        @source = RubiGen::PathSource.new(:app, file)
+        @types = @source.names(:visible)
+        @label = @source.label
       end
     end
     
