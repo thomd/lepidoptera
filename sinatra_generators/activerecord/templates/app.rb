8b02290e8833ebfd05587fa064d92642e14d25bc
Thomas Duerr <thomduerr@gmail.com>
2010-12-11 23:21:32 +0100

    fixed sinatra-activerecords project template to allow underscored project names

--- sinatra_generators/activerecord/templates/app.rb
+++ sinatra_generators/activerecord/templates/app.rb
@@ -6,37 +6,37 @@ require 'haml'
 
 # Database Object Model
 
-ActiveRecord::Base.establish_connection(:adapter  => "sqlite3", :database => "<%= name %>.sqlite3")
+ActiveRecord::Base.establish_connection(:adapter  => "sqlite3", :database => "<%= name.pluralize %>.sqlite3")
 
 class Migrate < ActiveRecord::Migration
 	def self.up
-		create_table :<%= name %>s do |t|
+		create_table :<%= name.pluralize %> do |t|
 			t.text :body
 			t.string :title
 		end
 	end
 	
 	def self.down
-		drop_table :<%= name %>s
+		drop_table :<%= name.pluralize %>s
 	end
 end
 
-class <%= name.camelize %> < ActiveRecord::Base
+class <%= name.classify %> < ActiveRecord::Base
 end
 
-Migrate.up unless <%= name.camelize %>.table_exists?
+Migrate.up unless <%= name.classify %>.table_exists?
 
 
 # Controller
 
 get '/' do
-	@<%= name %>s = <%= name.camelize %>.find(:all)
+	@<%= name.pluralize %> = <%= name.classify %>.find(:all)
 	haml :index
 end
 
 post '/' do
-		<%= name.camelize %>.delete(<%= name.camelize %>.find(:first)) until <%= name.camelize %>.count(:all) < 5
-		@<%= name %> = <%= name.camelize %>.new(:title => params[:title], :body => params[:body])
+		<%= name.classify %>.delete(<%= name.classify %>.find(:first)) until <%= name.classify %>.count(:all) < 5
+		@<%= name %> = <%= name.classify %>.new(:title => params[:title], :body => params[:body])
 		@<%= name %>.save
 		redirect '/'
 end
